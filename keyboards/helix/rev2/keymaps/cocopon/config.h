aee04e3623c0ac092d14c89414fa6f3a51ea813d
cocopon <cocopon@me.com>
2018-08-19 14:18:09 +0900

    Hello Helix!

---
+++ keyboards/helix/rev2/keymaps/cocopon/config.h
@@ -0,0 +1,37 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+// place overrides here
+
+// Mouse keys configuration
+#define MOUSEKEY_DELAY             0
+#define MOUSEKEY_MOVE_DELTA        8
+#define MOUSEKEY_INTERVAL          10
+#define MOUSEKEY_MAX_SPEED         2
+#define MOUSEKEY_TIME_TO_MAX       40
+#define MOUSEKEY_WHEEL_DELTA       1
+#define MOUSEKEY_WHEEL_MAX         1
+#define MOUSEKEY_WHEEL_MAX_SPEED   1
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 255
+
+#endif /* CONFIG_USER_H */
