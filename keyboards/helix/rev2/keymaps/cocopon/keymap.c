2904f6393c21cc0358ce70713da51f51d6360f9d
cocopon <cocopon@me.com>
2018-08-24 20:21:47 +0900

    Update key mappings

--- keyboards/helix/rev2/keymaps/cocopon/keymap.c
+++ keyboards/helix/rev2/keymaps/cocopon/keymap.c
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty */
   [_QWERTY] = LAYOUT( \
-      KC_GRV,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_QUOT, \
+      KC_GRV,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC, \
       M_LCTL,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_ENT,  \
       KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                         KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  M_RSFT,  \
       _______,  _______,  _______,  KC_LALT,  LOWER,    KC_LGUI,  KC_SPC,   KC_SPC,   KC_RGUI,  RAISE,    KC_RALT,  _______,  _______,  _______  \
@@ -157,17 +157,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Lower */
   [_LOWER] = LAYOUT( \
-      KC_ESC,   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                      KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_BSPC, \
-      KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                        _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE, \
-      KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,                       _______,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_EQL,  \
-      _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_ESC,   KC_ESC,   _______,  _______,  _______,  _______,  _______  \
+      KC_ESC,   KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                      KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_PIPE, \
+      _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        _______,  _______,  _______,  KC_LCBR,  KC_RCBR,  KC_QUOT, \
+      _______,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,                       _______,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_EQL,  \
+      _______,  KC_F11,   KC_F12,   _______,  _______,  _______,  _______,  KC_ESC,   KC_ESC,   _______,  _______,  _______,  _______,  _______  \
       ),
 
   /* Raise */
   [_RAISE] = LAYOUT( \
-      KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC, \
-      _______,  KC_PLUS,  KC_MINS,  KC_ASTR,  KC_SLSH,  KC_EQL,                       _______,  _______,  _______,  _______,  _______,  KC_BSLS, \
-      _______,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,                       KC_F12,   _______,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_EQL,  \
+      KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSLS, \
+      _______,  KC_PLUS,  KC_MINS,  KC_ASTR,  KC_SLSH,  KC_EQL,                       _______,  _______,  _______,  _______,  _______,  KC_QUOT, \
+      _______,  _______,  _______,  _______,  _______,  _______,                      KC_F12,   _______,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_EQL,  \
       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______  \
       ),
 
